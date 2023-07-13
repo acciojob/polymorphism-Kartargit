b2408c7027dcab544b41fece346a71f15a91b43e
Kartargit <skartar61962@gmail.com>
2023-07-13 12:53:26 +0530

    Task3_and_Task4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,11 +5,17 @@ public class Main {
      public int product(int x, int y){
          return x*y;
      }
-
+     public int product(int x, int y, int z){
+         return x*y*z ;
+     }
+     public double product(double x, double y){
+         return x*y;
+     }
  }
  public static void main(String[] args){
      Product p = new Product();
       System.out.println(p.product(6,7));
-
+     System.out.println(p.product(6,7,3));
+     System.out.println(p.product(5.5,9.5));
  }
 }
